4643f9107b00ac2af3f5382f90e3c35e235387ad
AnjaliPurohit2811 <anjalipurohit2811@gmail.com>
2024-02-08 09:53:12 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:design/bolt.dart';
+import 'package:design/splitter.dart';
+import 'package:design/wall.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WallScreen(),
+    ),
+  );
+}
